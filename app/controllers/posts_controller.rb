e96b887f2efb002b94085ebf679317bca519d94e
jonathanboss17 <jtboss47@gmail.com>
2019-08-12 20:43:16 -0400

    done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,18 +12,32 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+		# strong params
+		# only allows updates to title and description
+	  @post = Post.new(post_params(:title, :descriptions))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	#   strong params
+	#   only allows updates to title 
+	  @post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
 
 	def edit
 	  @post = Post.find(params[:id])
+	end
+
+	private 
+
+	# pass in in *args b/c this allows abstraction in the post_params method
+	# so we can specify which fields the users are allowed to edit
+	def post_params(*args)
+		# strong params ...
+		# can simply call post_params in the update and create method
+		params.require(:post).permit(*args)
 	end
 end
